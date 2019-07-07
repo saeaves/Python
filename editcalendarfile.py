e46561e7fae75f64b000912894b17eda67780a54
XACTDOM\I52975 <saeaves@xactware.com>
2019-07-07 12:33:53 -0600

    Added comments, and python version

--- editcalendarfile.py
+++ editcalendarfile.py
@@ -1,4 +1,5 @@
-#This program opens .ics files and adds notifications to each calendar option listed.
+#!/usr/bin/env python3
+#Copies a specified .ics file and adds notifications to each calendar option listed.
 import os
 import fileinput
 
@@ -10,10 +11,12 @@ try:
 except:
 	print("File not found!")
 	
-for line in infile: #fileinput.input(f"{fileName}.ics", inplace = 1):
+for line in infile:
+	#Replaces END:VEVENT, so it needs to be included.
 	if line.startswith("END:VEVENT"):
 		#Add ALARM, -P1DT12H0M0S is for 1 Day 12 Hours before
 		print("BEGIN:VALARM\nACTION:DISPLAY\nDESCRIPTION:This is an event reminder\nTRIGGER:-P1DT12H0M0S\nEND:VALARM\nEND:VEVENT", file=outfile)
+	#else, copy line
 	else:
 		print(line.rstrip(), file=outfile)
 outfile.close()	
